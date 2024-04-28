a235414bc498b96587f4b9e647e7f2a685a0cc93
JohanDevia <johandeviag@gmail.com>
2024-04-27 21:24:50 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
 import Feedbacks from './Feedbacks';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <div>
       <Feedbacks></Feedbacks>
     </div>
   );
